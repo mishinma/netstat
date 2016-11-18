78b3639464402315e1ec3ad19f3a678e024fdf08
Mikhail Mishin <mishinma1805@gmail.com>
2016-11-18 11:11:31 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='newstat',
+      version='0.1',
+      description='Data Mining project',
+      author='Mikhail Mishin, Max Reuter',
+      packages=[
+          'netstat'
+      ],
+      )
