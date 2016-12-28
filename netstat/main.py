71ff7f897f131d0a650a07697a0ff028720a9e6d
Max Reuter <max@Maxs-MBP.fritz.box>
2016-12-28 23:50:06 +0100

    removed hard coded values

--- netstat/main.py
+++ netstat/main.py
@@ -72,11 +72,11 @@ if __name__ == '__main__':
     else:
         lcc = pol.get_lcc(graph, connection='weak')
     start_time = time.time()
-    chunked_dist_distr = get_distance_distribution_parallel(lcc, False)
+    chunked_dist_distr = get_distance_distribution_parallel(lcc, directed)
     dist_distr = merge_distributions(chunked_dist_distr)
     elapsed = (time.time() - start_time)
     print '--- {} m ---'.format(elapsed / 60)
 
-    write_to_file(fname, dist_distr, False)
+    write_to_file(fname, dist_distr, directed)
 
     print 'Done'
