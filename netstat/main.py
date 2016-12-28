9e093dec9e10b9f061d57242f98b8968aea00e41
Max Reuter <max@Maxs-MBP.fritz.box>
2016-12-28 23:48:44 +0100

    clean-up main.py

--- netstat/main.py
+++ netstat/main.py
@@ -3,8 +3,7 @@ import time
 import sys
 import h5py
 import numpy as np
-
-from netstat import polygon as pol
+import polygon as pol
 
 
 def worker(graph, chunk, directed, output):
@@ -12,49 +11,72 @@ def worker(graph, chunk, directed, output):
     output.put(dist_distr)
 
 
-if __name__ == '__main__':
-    fname = sys.argv[1]
-    num_nodes, num_edges, graph_data = pol.load_graph_data(fname)
-    graph = pol.build_graph(num_nodes, num_edges, graph_data)
-    lcc = pol.get_lcc(graph, connection='weak')
-    nodes = np.arange(lcc.shape[0])
+def write_to_file(fname, data, directed=True):
+    f = h5py.File('distance_distributions', 'w')
+    network_name = fname.split('.')[0]
+
+    if network_name.endswith('-clean'):
+        network_name = network_name[:-6]
+    if directed:
+        dname = network_name + '-dir'
+    else:
+        dname = network_name + '-undir'
+
+    f.create_dataset(dname, data=data)
+    f.flush()
+    f.close()
+
+
+def merge_distributions(queue):
+    dist_distr = np.zeros(shape=100, dtype=np.int32)
+
+    while not queue.empty():
+        chunk_dist = queue.get_nowait()
+        if chunk_dist.size > dist_distr.size:
+            dist_distr.resize(chunk_dist.shape)
+        dist_distr[:chunk_dist.size] += chunk_dist
+
+    dist_distr_trimmed = np.trim_zeros(dist_distr, 'b')
+    return dist_distr_trimmed
+
+
+def get_distance_distribution_parallel(graph, directed=True):
+    nodes = np.arange(graph.shape[0])
     # Shared queue for output
     distributions = multiprocessing.Queue()
     cores = multiprocessing.cpu_count()
     processes = []
     # Split evenly into chunks for each available cpu
     chunks = np.array_split(nodes, cores)
-    start_time = time.time()
 
     for i in range(cores):
-        proc = multiprocessing.Process(target=worker, args=(lcc, chunks[i], False, distributions))
+        proc = multiprocessing.Process(target=worker, args=(graph, chunks[i], directed, distributions))
         processes.append(proc)
         proc.start()
 
     for proc in processes:
         proc.join()
-    print "Finished calculation in %s m" % str((time.time()-start_time)/60)
-    print "merging outputs..."
-    dist_distr = np.zeros(shape=100, dtype=np.int32)
 
-    while not distributions.empty():
-        chunk_dist = distributions.get_nowait()
-        if chunk_dist.size > dist_distr.size:
-            dist_distr.resize(chunk_dist.shape)
-        dist_distr[:chunk_dist.size] += chunk_dist
+    return distributions
 
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    directed = False
+    if len(sys.argv) > 2:
+        directed = sys.argv[2] == 'dir'
+    num_nodes, num_edges, graph_data = pol.load_graph_data(fname)
+    graph = pol.build_graph(num_nodes, num_edges, graph_data)
+    if directed:
+        lcc = pol.get_lcc(graph)
+    else:
+        lcc = pol.get_lcc(graph, connection='weak')
+    start_time = time.time()
+    chunked_dist_distr = get_distance_distribution_parallel(lcc, False)
+    dist_distr = merge_distributions(chunked_dist_distr)
     elapsed = (time.time() - start_time)
-    print "--- {} m ---".format(elapsed / 60)
-    dist_distr_trimmed = np.trim_zeros(dist_distr, 'b')
-    print dist_distr_trimmed
+    print '--- {} m ---'.format(elapsed / 60)
 
-    f = h5py.File('distance_distributions', 'w')
-    network_name = fname.split('.')[0]
-    if network_name.endswith('-clean'):
-        network_name = network_name[:-6]
-    dname = network_name + '-undir'
-    f.create_dataset(dname, data=dist_distr)
-    f.flush()
-    f.close()
+    write_to_file(fname, dist_distr, False)
 
-    print "Done"
+    print 'Done'
