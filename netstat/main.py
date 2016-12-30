b44f11a97928a3279328ec89e9158ce50dd8897d
Max Reuter <max.a.reuter@gmail.com>
2016-12-30 12:31:18 +0100

    add functions to compute statistics

--- netstat/main.py
+++ netstat/main.py
@@ -85,6 +85,43 @@ def sample_random_sources(graph, k, directed):
     return dist_distr_trimmed
 
 
+def get_distance_of_index(index, dist_distr):
+    i = 0
+    upper_bound = dist_distr[i]
+    while index > upper_bound:
+        i += 1
+        upper_bound += dist_distr[i]
+    # Index i represents distance i+1
+    return i+1
+
+
+def compute_median(dist_distr):
+    median_index = np.median(np.arange(np.sum(dist_distr)))
+    is_integer = median_index == int(median_index)
+    dist = get_distance_of_index(int(median_index), dist_distr)
+    if is_integer:
+        return dist
+    else:
+        return (2*dist+1)/2.0
+
+
+def compute_eff_diameter(dist_distr):
+    eff_diameter_index = np.percentile(np.arange(np.sum(dist_distr)), 90, interpolation='linear')
+    dist = get_distance_of_index(int(eff_diameter_index), dist_distr)
+    return dist
+
+
+def compute_statistics(dist_distr):
+    mean = np.sum([(i+1)*dist_distr[i] for i in xrange(dist_distr.size)])/float(np.sum(dist_distr))
+    median = compute_median(dist_distr)
+    diameter = dist_distr.size
+    eff_diameter = compute_eff_diameter(dist_distr)
+    print 'Mean: %d' % mean
+    print 'Median: %d' % median
+    print 'Diameter: %d' % diameter
+    print 'Effective Diameter: %d' % eff_diameter
+
+
 if __name__ == '__main__':
     fname = sys.argv[1]
     directed = False
@@ -97,14 +134,13 @@ if __name__ == '__main__':
     else:
         lcc = pol.get_lcc(graph, connection='weak')
     start_time = time.time()
-    dist_distr_multi = get_distance_distribution_parallel(lcc, directed)
-    # dist_distr_single = np.trim_zeros(pol.get_distance_distribution(lcc, directed=directed), 'b')
+    dist_distr = get_distance_distribution_parallel(lcc, directed)
     # dist_distr = sample_random_pairs(lcc, 100, directed)
     # dist_distr = sample_random_sources(lcc, 100, directed)
-    print dist_distr_multi
-    # print dist_distr_single
+    print dist_distr
+    compute_statistics(dist_distr)
     elapsed = (time.time() - start_time)
     print '--- {} m ---'.format(elapsed / 60)
-    # write_to_file(fname, dist_distr, directed)
+    write_to_file(fname, dist_distr, directed)
 
     print 'Done'
