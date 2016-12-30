eb2e30133782757aae09ccd75fde5bd420893a4b
Mikhail Mishin <mishinma1805@gmail.com>
2016-12-30 22:50:28 +0200

    Modify the clean script
    - Use strings instead of numbers as dict keys
    - Remove duplicate items when sorting
    - Count the number of edges after doing that

--- netstat/clean.py
+++ netstat/clean.py
@@ -30,7 +30,6 @@ def clean_file(fname_old, fname_new):
         with open(fname_new, 'w') as f_new:
 
             clnr = Cleaner()
-            edge_cnt = 0
 
             for line in f_old:
 
@@ -38,18 +37,25 @@ def clean_file(fname_old, fname_new):
                 if line.startswith('#'):
                     continue
 
-                id_from, id_to = map(int, line.split())
+                id_from, id_to = line.split()
                 id_from_new = clnr(id_from)
                 id_to_new = clnr(id_to)
                 f_new.write("{} {}\n".format(id_from_new, id_to_new))
-                edge_cnt += 1
 
-    num_nodes = clnr.vtx_cnt
-    num_edges = edge_cnt
+
+
 
     subprocess.call(
-        "sort -n -k1,1 -k2,2 -o {fname_new} {fname_new}".format(fname_new=fname_new), shell=True
+        "sort -n -u -k1,1 -k2,2 -o {fname_new} {fname_new}".format(fname_new=fname_new), shell=True
     )
+
+    num_nodes = clnr.vtx_cnt
+
+    with open(fname_new, 'r') as f_new:
+        for i, l in enumerate(f_new, start=1):
+            pass
+    num_edges = i
+
 
     subprocess.call(
         "echo '{num_nodes} {num_edges}' | cat - {fname_new} > temp && mv temp {fname_new}" \
