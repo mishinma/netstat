13e4752d0eac648614f187761751649d40130a64
Mikhail Mishin <mishinma1805@gmail.com>
2016-12-30 19:46:20 +0200

    Rewrite statistics

--- netstat/statistics.py
+++ netstat/statistics.py
@@ -3,38 +3,39 @@
 import numpy as np
 
 
-def get_distance_of_index(index, dist_distr):
-    i = 0
-    upper_bound = dist_distr[i]
-    while index > upper_bound:
-        i += 1
-        upper_bound += dist_distr[i]
-    # Index i represents distance i+1
-    return i+1
+def normalize(distr):
+    return distr*1.0/np.sum(distr)
 
 
-def compute_median(dist_distr):
-    median_index = np.median(np.arange(np.sum(dist_distr)))
-    is_integer = median_index == int(median_index)
-    dist = get_distance_of_index(int(median_index), dist_distr)
-    if is_integer:
-        return dist
-    else:
-        return (2*dist+1)/2.0
+def mean(distr):
+    distr_zip = np.vstack((np.arange(distr.size), distr)).T  # Zip indices and values
+    mn = np.sum(np.prod(distr_zip, axis=1))
+    return mn
 
 
-def compute_eff_diameter(dist_distr):
-    eff_diameter_index = np.percentile(np.arange(np.sum(dist_distr)), 90, interpolation='linear')
-    dist = get_distance_of_index(int(eff_diameter_index), dist_distr)
-    return dist
+def quantile(distr, phi):
+    distr_cmf = np.cumsum(distr)
+    distr_ge_phi = np.logical_or(np.isclose(distr_cmf, phi), distr_cmf > phi)
+    return np.argmax(distr_ge_phi)
 
 
-def compute_statistics(dist_distr):
-    mean = np.sum([(i+1)*dist_distr[i] for i in xrange(dist_distr.size)])/float(np.sum(dist_distr))
-    median = compute_median(dist_distr)
-    diameter = dist_distr.size
-    eff_diameter = compute_eff_diameter(dist_distr)
-    print 'Mean: %d' % mean
-    print 'Median: %d' % median
-    print 'Diameter: %d' % diameter
-    print 'Effective Diameter: %d' % eff_diameter
+def median(distr):
+    return quantile(distr, 0.5)
+
+
+def effective(distr):
+    return quantile(distr, 0.9)
+
+
+def mode(distr):
+    return distr.size - 1
+
+
+def get_statistics(distr):
+    distr = normalize(distr)
+    return (
+        mean(distr),
+        median(distr),
+        mode(distr),
+        effective(distr)
+    )
