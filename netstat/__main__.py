bc2cec43c27d6fffcd13c34b3504e0ae0be4bb28
Mikhail Mishin <mishinma1805@gmail.com>
2016-12-31 19:19:15 +0200

    Add basic logic

--- netstat/__main__.py
+++ netstat/__main__.py
@@ -1,20 +1,27 @@
 """ Main script """
 import argparse
 
+EXACT = 'e'
+APPROX = 'a'
 
 def run_exact(graph, directed=True):
     pass
 
+def run_approx(graph, directed=True):
+    pass
+
 parser = argparse.ArgumentParser()
 parser.add_argument("fname", help="Filename to read")
-parser.add_argument("-m", "--mode", choices=["e",  "a"])
+parser.add_argument("-m", "--mode", choices=[EXACT, APPROX])
 # group = parser.add_mutually_exclusive_group()
 # group.add_argument("-e", "--exact", action="store_true",
 #                    help="Exact computation")
 # group.add_argument("-a", "--approx", action="store true",
 #                    help="Approximate computation")
-parser.add_argument('--example', nargs='?', const=1, type=int, default=1)
 
 args = parser.parse_args()
-if args.verbosity:
-    print "verbosity turned on"
+
+if args.mode is EXACT:
+    run_exact()
+else:
+    run_approx()
