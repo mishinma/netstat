3d0a54b97664e22e54960cf90ee31527a078f9c5
Max Reuter <max.reuter@aalto.fi>
2017-01-01 16:50:00 +0100

    add warning message for mac

--- install_venv.py
+++ install_venv.py
@@ -11,10 +11,15 @@ path = os.path.abspath("sw")
 assert os.path.exists(path)
 
 win32 = sys.platform.startswith('win')
+mac = sys.platform == 'darwin'
 
 if win32:
     raise OSError("Windows is not supported")
 else:
+    if mac:
+        print "Mac platform detected. In case of compilation issues, try exporting numpy's path to "\
+        "CFLAGS and run again. Example:\n"\
+        "export CFLAGS=\"-I /usr/local/lib/python2.7/site-packages/numpy/core/include $CFLAGS\""
     os.system("%s virtualenv-15.1.0/virtualenv.py --system-site-packages .venv" % sys.executable)
     os.system(".venv/bin/pip install -r requirements.txt")
     # Packages also available in sw
