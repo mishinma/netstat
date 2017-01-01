a939d1d3cc588f9a6666b9f59da526918b4a0dd2
Mikhail Mishin <mishinma1805@gmail.com>
2017-01-01 21:41:38 +0200

    Use numpy for reading files

--- netstat/util.py
+++ netstat/util.py
@@ -1,7 +1,7 @@
 """ Module for helper functions """
 import time
 import numpy as np
-import pandas as pd
+# import pandas as pd
 
 from math import ceil, log
 from scipy.sparse import csr_matrix, csgraph
@@ -25,8 +25,11 @@ def load_graph_data(fname):
                                   " or use script `clean` to format the file.")
         num_nodes, num_edges = map(int, next(f).split())
 
-    edges = pd.read_csv(fname, sep=' ', names=['FromNodeId', 'ToNodeId'],
-                             dtype={'FromNodeId': np.int32, 'ToNodeId': np.int32}, skiprows=2)
+    edges = np.loadtxt(fname, dtype=np.int32, skiprows=2)
+
+    # Pandas's function is much faster
+    # edges = pd.read_csv(fname, sep=' ', names=['FromNodeId', 'ToNodeId'],
+    #                          dtype={'FromNodeId': np.int32, 'ToNodeId': np.int32}, skiprows=2)
 
     return num_nodes, num_edges, edges
 
@@ -34,10 +37,11 @@ def load_graph_data(fname):
 def build_graph(num_nodes, num_edges, edges):
     """ Create a sparse CSR matrix representing the graph """
 
-    edges = edges.values  # Access values as np array
+    # If `edges` is a DataFrame
+    # edges = edges.values  # Access values as np array
 
-    row = edges[:, 0]
-    col = edges[:, 1]
+    row = edges[:, 0].copy(order='C')
+    col = edges[:, 1].copy(order='C')
 
     row_counts = np.bincount(row, minlength=num_nodes)
     indptr = np.hstack([0, np.cumsum(row_counts)])
@@ -52,8 +56,8 @@ def build_graph(num_nodes, num_edges, edges):
 def largest_connected_component(graph, connection='strong'):
     """ Get the largest connected component and return it as a submatrix """
 
-
     num_components, labels = csgraph.connected_components(graph, connection=connection)
+
     lcc_label = np.argmax(np.bincount(labels))  # Find the largest label
     indices = np.nonzero(labels == lcc_label)[0]  # Find the corresponding indices
 
